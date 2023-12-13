c51e51d455442b962797061bd9528cba94c0084c
chibi929 <ibihc929@gmail.com>
2023-12-14 08:55:52 +0900

    [Brefore 'Chapter 12'] Auto-formatter.

--- src/app/ui/invoices/create-form.tsx
+++ src/app/ui/invoices/create-form.tsx
@@ -1,12 +1,8 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
-import {
-  CheckIcon,
-  ClockIcon,
-  CurrencyDollarIcon,
-  UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
+import Link from 'next/link'
+
+import { CustomerField } from '@/app/lib/definitions'
+import { Button } from '@/app/ui/button'
+import { CheckIcon, ClockIcon, CurrencyDollarIcon, UserCircleIcon } from '@heroicons/react/24/outline'
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
@@ -59,9 +55,7 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
 
         {/* Invoice Status */}
         <fieldset>
-          <legend className="mb-2 block text-sm font-medium">
-            Set the invoice status
-          </legend>
+          <legend className="mb-2 block text-sm font-medium">Set the invoice status</legend>
           <div className="rounded-md border border-gray-200 bg-white px-[14px] py-3">
             <div className="flex gap-4">
               <div className="flex items-center">
@@ -108,5 +102,5 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
         <Button type="submit">Create Invoice</Button>
       </div>
     </form>
-  );
+  )
 }
