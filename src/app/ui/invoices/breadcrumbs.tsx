a9d75501bc3d97fc4e2ab0678182ba8c44a8e9c6
chibi929 <ibihc929@gmail.com>
2023-12-14 09:03:24 +0900

    [Update] Commit the 'Chapter 12' - Mutating Data.

--- src/app/ui/invoices/breadcrumbs.tsx
+++ src/app/ui/invoices/breadcrumbs.tsx
@@ -1,7 +1,7 @@
 import { clsx } from 'clsx'
 import Link from 'next/link'
 
-import { lusitana } from '@/app/ui/fonts'
+import { lusitana } from '@/configs/fonts'
 
 interface Breadcrumb {
   label: string
