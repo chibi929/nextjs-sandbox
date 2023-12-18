09aa26c7770aa62bfe509575d55592347cb54520
chibi929 <ibihc929@gmail.com>
2023-12-18 22:54:35 +0900

    [Setup - NextUI] Setup the NextUI modules.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,14 @@ import '@/styles/globals.css'
 
 import { inter } from '@/configs/fonts'
 
+import { Providers } from './providers'
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
+    <html lang="en" className="light">
+      <body className={`${inter.className} antialiased`}>
+        <Providers>{children}</Providers>
+      </body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,20 @@
 import { NextPage } from 'next'
 
+import { Button } from '@nextui-org/react'
+
 const Page: NextPage = () => {
-  return <div>Index</div>
+  return (
+    <main>
+      <div className="flex flex-wrap gap-4 items-center">
+        <Button color="default">Default</Button>
+        <Button color="primary">Primary</Button>
+        <Button color="secondary">Secondary</Button>
+        <Button color="success">Success</Button>
+        <Button color="warning">Warning</Button>
+        <Button color="danger">Danger</Button>
+      </div>
+    </main>
+  )
 }
 
 export default Page
