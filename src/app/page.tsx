9535a125fcb8cfefd0967fa8e00632dea88c17c1
chibi929 <ibihc929@gmail.com>
2023-12-19 23:51:05 +0900

    [Update] Create sandbox page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,7 @@
 import { NextPage } from 'next'
 
-import QRCode from '@/components/sandbox/QRCode'
-import { Button, Card, CardBody, CardHeader } from '@nextui-org/react'
-
 const Page: NextPage = () => {
-  return (
-    <main className="container mx-auto px-4">
-      <div className="flex flex-wrap gap-4 items-center">
-        <Card>
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">nextui</h4>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <Button color="default">Default</Button>
-            <Button color="primary">Primary</Button>
-            <Button color="secondary">Secondary</Button>
-            <Button color="success">Success</Button>
-            <Button color="warning">Warning</Button>
-            <Button color="danger">Danger</Button>
-          </CardBody>
-        </Card>
-      </div>
-      <div className="flex flex-wrap gap-4 items-center">
-        <Card>
-          <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
-            <h4 className="font-bold text-large">react.qrcode</h4>
-          </CardHeader>
-          <CardBody className="overflow-visible py-2">
-            <QRCode src="https://chibi929.github.io" />
-          </CardBody>
-        </Card>
-      </div>
-    </main>
-  )
+  return <main>Root Page</main>
 }
 
 export default Page
